572cc3c54f3627b54f6cee17ea51fb7717d17237
unknown <abigail_rick@mailfence.com>
2024-10-24 21:31:54 -0500

    added alt
    I didn't add any description to the alt

--- components/about.js
+++ components/about.js
@@ -5,7 +5,7 @@ export default function About (){
     return (
         <div id="about" className='about'>
             <div className='aboutContainer'>                
-                <img className='aboutImg' src='dogPicture1.jpg'></img>
+                <img className='aboutImg' alt="" src='dogPicture1.jpg'></img>
                 
                 <p className='aboutP'><span className='bold' id="aboutHeading">Meet your pet's new best friend! </span><br></br>
                 <br></br>Hi, I’m Abigail! I’ve been caring for dogs for over 10 years, with hands-on experience in grooming, training, and providing top-notch pet sitting. Whether your pup needs a quick walk, structured training, or a comfortable place to stay while you’re away, I’ve got you covered.
